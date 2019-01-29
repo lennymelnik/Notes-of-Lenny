00ae6c79bfb672c9fdf782a2922895303c516c9e
Leonard Melnik <leonard@onedr.io>
2019-01-29 00:44:28 -0500

    Started learning folder, started Java w/notes

---
+++ Learning/Java/Hello_World/Main.java
@@ -0,0 +1,11 @@
+public class Main { //define a class called name
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //static means you can run the method without creating an instance of public static void main(String[] args) {
+          //void means it dues not return any value
+          //main is the name of the m method
+    }
+}
+// In java all code that can actually be run is in a class.
+
+//public means any other class can access it
