8c13ec38b368d18cc561ef6065c14ef68fd21dfa
Leonard Melnik <leonard@onedr.io>
2019-01-30 13:57:52 -0500

    notes 1-30-19

---
+++ Learning/Java/Functions/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void foo(){
+
+  //do something here
+Student b = new Student();
+b.setName("Leonard");
+String name = b.getName();
+System.out.println(b.getName());
+//use with
+int a = 3;
+int b = 5;
+bar(a, b);
+  }
+
+//foo is the method that we defined in the class public static void main(String[] args) {
+  //Static means it eblongs to the class Main and not a speicifc instance of it. Which means we can call the method from a difference class with Main.foo(
+  //void means the method will not return a method
+
+//Arguments
+
+public void bar(int num1, int num2){
+
+}
+
+
+
+}
---
+++ Learning/Java/Functions/NonStatic/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+public static void main(String[] args) {
+
+    Student l = new Student();
+    l.setName("Leonard");
+    String name  = l.getName();
+    System.out.println(name);
+
+}
+}
