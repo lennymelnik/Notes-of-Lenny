9419d05cc799756de8d648a43e4dbc2134cc2604
Leonard Melnik <leonard@onedr.io>
2019-01-30 00:37:49 -0500

    Added arrays and loops, summed up pretty well

---
+++ Learning/Java/Loops/main.java
@@ -0,0 +1,16 @@
+//The for loop has three sections
+
+for (int i = 0/*is run when we enter the loop*/; i <3/*gatekeeper, if it returns true we runt eh statements in the loop, else it exists the loop*/; i++/*increment, runs every time the loop runs*/){
+
+
+}
+
+//The while sytac is very similar
+
+while (condition){
+  //the condition will run for the first time when entering and every time the loop is done. If it returns fase, the loop will not run.
+}
+
+
+//
+continue;//will stop the current iteration and move to the next one. But inside a for loop it still runs the third section
