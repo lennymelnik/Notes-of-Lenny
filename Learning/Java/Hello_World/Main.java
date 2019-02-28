e5ec0b7bf2b2350671bffd2b90bb209544064092
JosephYusufov <jyusufov00@stuy.edu>
2019-02-28 13:43:19 -0500

    daniel

--- Learning/Java/Hello_World/Main.java
+++ Learning/Java/Hello_World/Main.java
@@ -1,6 +1,7 @@
 public class Main { //define a class called name
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Mr.coolguy.123 wuz hear");
         //static means you can run the method without creating an instance of public static void main(String[] args) {
           //void means it dues not return any value
           //main is the name of the m method
